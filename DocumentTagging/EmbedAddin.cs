5741ba77e09d879ab8ee8303f55b46b6e10bd9f9
Xiangmin Ru <xiru@microsoft.com>
2022-09-21 13:47:14 +0800

    First commit.

---
+++ DocumentTagging/EmbedAddin.cs
@@ -0,0 +1,85 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System.IO;
+
+namespace DocumentTagging
+{
+    public static class AddinEmbedder
+    {
+        // Embeds the add-in into a file of the specified type.
+        public static void EmbedAddin(string fileType, Stream memoryStream)
+        {
+            // Each Office file type has its own *Document class in the OOXML SDK.
+            switch (fileType)
+            {
+                case "Excel":
+                    using (var spreadsheet = SpreadsheetDocument.Open(memoryStream, true))
+                    {
+                        //spreadsheet.DeletePart(spreadsheet.WebExTaskpanesPart);
+                        if (spreadsheet.WebExTaskpanesPart != null)
+						{
+                            var webExTaskpanesPart = spreadsheet.WebExTaskpanesPart;
+                            int count = 1;
+                            foreach (var WebExtensionPart in webExTaskpanesPart.WebExtensionParts)
+                            {
+                                count++;
+                            }
+
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, count);
+                        } else
+						{
+                            var webExTaskpanesPart = spreadsheet.AddWebExTaskpanesPart();
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, 1);
+                        }
+                    }
+                    break;
+                case "Word":
+                    using (var document = WordprocessingDocument.Open(memoryStream, true))
+                    {
+                        if (document.WebExTaskpanesPart != null)
+						{
+                            var webExTaskpanesPart = document.WebExTaskpanesPart;
+                            int count = 1;
+                            foreach (var WebExtensionPart in webExTaskpanesPart.WebExtensionParts)
+							{
+                                count++;
+							}
+
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, count);
+
+                        } else
+						{
+                            var webExTaskpanesPart = document.AddWebExTaskpanesPart();
+
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, 1);
+                        }
+                    }
+                    break;
+                case "PowerPoint":
+                    using (var slidedeck = PresentationDocument.Open(memoryStream, true))
+                    {
+                        if (slidedeck.WebExTaskpanesPart != null)
+                        {
+                            var webExTaskpanesPart = slidedeck.WebExTaskpanesPart;
+                            int count = 1;
+                            foreach (var WebExtensionPart in webExTaskpanesPart.WebExtensionParts)
+                            {
+                                count++;
+                            }
+
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, count);
+
+                        }
+                        else
+                        {
+                            var webExTaskpanesPart = slidedeck.AddWebExTaskpanesPart();
+                            OOXMLHelper.CreateWebExTaskpanesPart(webExTaskpanesPart, 1);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+                    //throw new Exception("Invalid File Type");
+            }
+        }
+    }
+}
---
+++ DocumentTagging/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace DocumentTagging
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string sourcedir = ConfigurationManager.AppSettings.Get("sourcedir");
+			try
+			{
+				handleAllFiles(sourcedir, ConfigurationManager.AppSettings.Get("destdir"));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		static void handleAllFiles(string sPath, string dPath)
+		{
+			DirectoryInfo d = new DirectoryInfo(sPath);
+			FileSystemInfo[] fsinfos = d.GetFileSystemInfos();
+			if (!Directory.Exists(dPath))
+			{
+				Directory.CreateDirectory(dPath);
+			}
+
+			foreach (FileSystemInfo fsinfo in fsinfos)
+			{
+				if (fsinfo is DirectoryInfo)
+				{
+					handleAllFiles(fsinfo.FullName, dPath + "\\" + fsinfo.Name);
+				}
+				else
+				{
+					FileInfo fi = new FileInfo(fsinfo.FullName);
+					fi.CopyTo(dPath + "\\" + fi.Name, true);
+					FileStream fs = new FileStream(dPath + "\\" + fi.Name, FileMode.Open);
+					AddinEmbedder.EmbedAddin(getFileType(fsinfo), fs);
+					fs.Close();
+
+				}
+			}
+
+		}
+		static string getFileType(FileSystemInfo file)
+		{
+			switch (file.Extension.ToLower())
+			{
+				case ".xlsx":
+					return "Excel";
+				case ".docx":
+					return "Word";
+				case ".pptx":
+					return "PowerPoint";
+				default:
+					return null;
+			}
+		}
+	}
+}
